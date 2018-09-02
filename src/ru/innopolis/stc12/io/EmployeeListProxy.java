db30dadf9e2e251463f8367893d139b9f26a9cb8
RailKO <rail-ko@mail.ru>
2018-09-02 20:10:19 +0500

    1. added XML serialization, need debug

--- src/ru/innopolis/stc12/io/EmployeeListProxy.java
+++ src/ru/innopolis/stc12/io/EmployeeListProxy.java
@@ -1,7 +1,15 @@
 package ru.innopolis.stc12.io;
 
+import javax.xml.stream.*;
+import javax.xml.stream.events.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeListProxy implements InvocationHandler {
     private final String FILE_NAME = "employees.xml";
@@ -12,13 +20,107 @@ public class EmployeeListProxy implements InvocationHandler {
     }
 
     private boolean readXml() {
-        System.out.println("read from xml file");
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        List<Employee> employeeList = new ArrayList<>();
+        try {
+            XMLEventReader eventReader = inputFactory.createXMLEventReader(new FileInputStream(FILE_NAME));
+            Employee employee = null;
+
+            while (eventReader.hasNext()) {
+                XMLEvent event = eventReader.nextEvent();
+
+                if (event.isStartElement()) {
+                    String elementName = event.asStartElement().getName().getLocalPart();
+                    if (elementName.compareTo("employee") == 0) {
+                        employee = new Employee();
+                    }
+                    switch (elementName) {
+                        case "name":
+                            event = eventReader.nextEvent();
+                            employee.setName(event.asCharacters().getData());
+                            break;
+                        case "age":
+                            event = eventReader.nextEvent();
+                            employee.setAge(Integer.parseInt(event.asCharacters().getData()));
+                            break;
+                        case "salary":
+                            event = eventReader.nextEvent();
+                            employee.setSalary(Double.parseDouble(event.asCharacters().getData()));
+                            break;
+                        case "job":
+                            event = eventReader.nextEvent();
+                            employee.setJob(Job.valueOf(event.asCharacters().getData()));
+                            break;
+                    }
+                }
+
+                if (event.isEndElement()) {
+                    if (event.asEndElement().getName().getLocalPart().compareTo("employee") == 0) {
+                        employeeList.add(employee);
+                    }
+                }
+            }
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
     private boolean writeXml() {
-        System.out.println("write to xml file");
+        try {
+            Field field = mySerialization.getClass().getDeclaredField("employeeList");
+            field.setAccessible(true);
+            List<Employee> employeeList = (List<Employee>) field.get(mySerialization);//TODO instanceof?
+
+            XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+
+            XMLEventWriter eventWriter = outputFactory.createXMLEventWriter(new FileOutputStream(FILE_NAME));
+            XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+            XMLEvent end = eventFactory.createDTD("\n");
+
+            StartDocument startDocument = eventFactory.createStartDocument();
+            eventWriter.add(startDocument);
+            eventWriter.add(end);
+
+            for (Employee employee : employeeList) {
+                eventWriter.add(eventFactory.createStartElement("", "", "employee"));
+                eventWriter.add(end);
+
+                createNode(eventWriter, "name", employee.getName());
+                createNode(eventWriter, "age", String.valueOf(employee.getAge()));
+                createNode(eventWriter, "salary", String.valueOf(employee.getSalary()));
+                createNode(eventWriter, "job", employee.getJob().toString());
+
+                eventWriter.add(eventFactory.createEndElement("", "", "employee"));
+                eventWriter.add(end);
+            }
+
+            eventWriter.add(eventFactory.createEndDocument());
+            eventWriter.close();
+        } catch (NoSuchFieldException | IllegalAccessException | FileNotFoundException | XMLStreamException e) {
+            e.printStackTrace();
+        }
+
         return true;
+    }
+
+    private void createNode(XMLEventWriter eventWriter, String name, String value) throws XMLStreamException {
+        XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+        XMLEvent end = eventFactory.createDTD("\n");
+        XMLEvent tab = eventFactory.createDTD("\t");
+
+        StartElement startElement = eventFactory.createStartElement("", "", name);
+        eventWriter.add(tab);
+        eventWriter.add(startElement);
+
+        Characters characters = eventFactory.createCharacters(value);
+        eventWriter.add(characters);
+
+        EndElement endElement = eventFactory.createEndElement("", "", name);
+        eventWriter.add(endElement);
+        eventWriter.add(end);
     }
 
     @Override
--- src/ru/innopolis/stc12/io/Main.java
+++ src/ru/innopolis/stc12/io/Main.java
@@ -14,6 +14,19 @@ public class Main {
                 employeeList.getClass().getInterfaces(), new EmployeeListProxy(employeeList));
 
         mySerialization.readFromFile();
+
+        Employee employee1 = new Employee("Tom", 35, 35050.5, Job.Administrator);
+        Employee employee2 = new Employee("Alex", 25, 45050.5, Job.Economist);
+        Employee employee3 = new Employee("Monica", 27, 25050.5, Job.Economist);
+        Employee employee4 = new Employee("Rose", 29, 55050.5, Job.Programmer);
+        Employee employee5 = new Employee("Jon", 31, 155050.5, Job.Director);
+
+        employeeList.save(employee1);
+        employeeList.save(employee2);
+        employeeList.save(employee3);
+        employeeList.save(employee4);
+        employeeList.save(employee5);
+
         mySerialization.saveToFile();
     }
 
